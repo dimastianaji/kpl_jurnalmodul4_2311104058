0191e91e970f415047a2cd1a4ca0890b2a1ac5a4
Dimastian Aji Wibowo <dimastian.exe@gmail.com>
2025-03-05 05:45:45 +0700

    menambahkan class KodeBuah

---
+++ kpl_jurnalmodul4_2311104058/KodeBuah.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kpl_jurnalmodul4_2311104058
+{
+    class KodeBuah
+    {
+        private Dictionary<string, string> kodeBuah = new Dictionary<string, string>
+    {
+        {"Apel", "A00"},
+        {"Aprikot", "B00"},
+        {"Alpukat", "C00"},
+        {"Pisang", "D00"},
+        {"Paprika", "E00"},
+        {"Blackberry", "F00"},
+        {"Ceri", "H00"},
+        {"Kelapa", "I00"},
+        {"Jagung", "J00"}
+    };
+
+        public string getKodeBuah(string namaBuah)
+        {
+            if (kodeBuah.ContainsKey(namaBuah))
+            {
+                return kodeBuah[namaBuah];
+            }
+            else
+            {
+                return "Kode tidak ditemukan";
+            }
+        }
+    }
+}
--- kpl_jurnalmodul4_2311104058/Program.cs
+++ kpl_jurnalmodul4_2311104058/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using kpl_jurnalmodul4_2311104058;
+
+class Program
+{
+    static void Main()
+    {
+        KodeBuah kb = new KodeBuah();
+        Console.Write("Masukkan nama buah: ");
+        string namaBuah = Console.ReadLine();
+        Console.WriteLine("Kode Buah: " + kb.getKodeBuah(namaBuah));
+    }
+}
